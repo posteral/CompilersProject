4334ef2411232a9258082070993fb0f87ea6f6b4
iflopes <ingrid.fleschlopes@gmail.com>
2014-03-08 12:58:13 -0300

    Dictionary data structure definitions added

--- include/comp_dict.h
+++ include/comp_dict.h
@@ -1,7 +1,16 @@
+#define MAX_HASH_SLOT 10000
+
 typedef struct comp_dict_item_t {
+        struct comp_dict_item_t* next;
+        struct comp_dict_item_t* previous;
+        void* data;            
+        const char* key;  
 } comp_dict_item_t;
 
 typedef struct comp_dict_t {
+        comp_dict_item_t* hash[MAX_HASH_SLOT]; 
+        comp_dict_item_t* start;               
+        comp_dict_item_t* end; 
 } comp_dict_t;
 
-comp_dict_t* dictCreate();
+struct comp_dict_t* dictCreate();
