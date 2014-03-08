860f7ced3e646835f37c8bb2ef427b8f07b01747
iflopes <ingrid.fleschlopes@gmail.com>
2014-03-08 14:00:19 -0300

    Add and remove item functions added

--- src/comp_dict.c
+++ src/comp_dict.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include "comp_dict.h"
 
-comp_dict_t* dictCreate(void){
+struct comp_dict_t* dictCreate(void){
 	comp_dict_t *newDict;
 	
 	newDict = (comp_dict_t*)malloc(sizeof(comp_dict_t));
@@ -9,3 +9,58 @@ comp_dict_t* dictCreate(void){
 	return newDict;
 }
 
+void dictAddItem(struct comp_dict_t *dictionary, const char *key, void *data){
+     comp_dict_item_t *item = (comp_dict_item_t *)malloc(sizeof(comp_dict_item_t));
+     item->data = data;
+     item->next = NULL;
+     
+     if (dictionary->start == NULL) {        
+        item->previous = NULL;
+     }   
+     else {
+          item->previous = dictionary->end;
+          dictionary->end->next = item;
+          }
+          
+     dictionary->hash[dictGetHashValue(key)] = item;
+     dictionary->end = item; 
+}
+
+int dictRemoveItem(struct comp_dict_t *dictionary, const char *key){ 
+     comp_dict_item_t *removed_element;   
+
+     if(dictionary->start == NULL)
+     { 
+       printf("WARNING: There is no element with this key in the dictionary!");
+       return -1;   
+     }
+     
+     // First element
+     if(dictionary->hash[dictGetHashValue(key)]->previous == NULL) 
+     {   
+         removed_element = dictionary->hash[dictGetHashValue(key)];   
+         dictionary->start = dictionary->hash[dictGetHashValue(key)]->next;   
+         if(dictionary->start == NULL)  
+                              dictionary->end = NULL; 
+         else   
+                              dictionary->start->previous = NULL; 
+     }
+     else 
+          // Last element
+          if(dictionary->hash[dictGetHashValue(key)]->next == NULL) 
+          {   
+            removed_element = dictionary->hash[dictGetHashValue(key);   
+            dictionary->end->previous->next = NULL;   
+            dictionary->end = dictionary->end->previous;   
+          }
+          // Otherwise
+          else 
+          {   
+              dictionary->hash[dictGetHashValue(key)]->previous->next = dictionary->hash[dictGetHashValue(key)]->next;   
+              dictionary->hash[dictGetHashValue(key)]->next->previous = dictionary->hash[dictGetHashValue(key)]->previous;   
+          }
+             
+     free(removed_element->data);   
+     free(removed_element);     
+     return 0;
+}
