8c4143a5ae4f51d457f30ee04b15119cc767ac68
iflopes <ingrid.fleschlopes@gmail.com>
2014-03-09 10:42:07 -0300

    comp_dict.h: Updating changes in comp_dict.c

--- include/comp_dict.h
+++ include/comp_dict.h
@@ -1,9 +1,10 @@
 #define MAX_HASH_SLOT 10000
+#define HASH_SIZE 101
 
 typedef struct comp_dict_item_t {
         struct comp_dict_item_t* next;
         struct comp_dict_item_t* previous;
-        void* data;            
+        const char* data;            
         const char* key;  
 } comp_dict_item_t;
 
@@ -14,3 +15,9 @@ typedef struct comp_dict_t {
 } comp_dict_t;
 
 struct comp_dict_t* dictCreate();
+void                dictAddItem(struct comp_dict_t *dictionary, const char *key, const char *data);
+int                 dictEditItem(struct comp_dict_t *dictionary, const char *key, const char *new_data);
+int                 dictRemoveItem(struct comp_dict_t *dictionary, const char *key);
+void                dictPrint(struct comp_dict_t *dictionary);
+void                dictEmpty(struct comp_dict_t *dictionary);
+int                 dictGetHashValue(const char *key);
