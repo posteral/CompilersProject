a972278c0bccd84cb99f2ae9a3c3cc32bbf6cf5d
posteral <filipe.posteral@gmail.com>
2014-03-10 12:29:19 -0300

    comp_list.c and comp_list.h: listDelete function added

--- include/comp_list.h
+++ include/comp_list.h
@@ -6,4 +6,5 @@ typedef struct comp_list_t_t {
 struct comp_list_t_t *listCreate();
 int		listEmpty(struct comp_list_t_t *head);
 void	listPush(struct comp_list_t_t  **head, int data);
+struct comp_list_t_t *listDelete(struct comp_list_t_t *head, int data);
 void	listPrint(struct comp_list_t_t *head);
